7459cb70488fdc5a93430a4e48c4961daa34d35f
haileyfisherr <hfisher1237@gmail.com>
2024-08-22 13:36:42 -0400

    Add files via upload

---
+++ program0_Hailey_Fisher.c
@@ -0,0 +1,27 @@
+//********************************************************
+// program-0.c
+// Author: Hailey Fisher
+// Date: 8/22/24
+// Class: COP 3223, Professor Parra
+// Purpose: This program prints a specific message to the
+// command line. The main purpose of this assignment is to
+// check and see whether I can connect to the Codespaces Server.
+// Input: None
+//
+// Output: (to the command line) A breif Message crafted by 
+// Dr. Moran to test running a C program on Codespaces
+// //********************************************************
+
+
+// Header file for input output functions
+#include <stdio.h>
+ 
+// main function - where we will print the message
+int main()
+{
+ 
+    // prints the message to the command line.
+    printf("The current month is August.\nThe current year is 2024.\nI am a student at UCF.\nThis Fall 2024 I am taking Intro to Programming in C with Benji.\nI am super exicted to learn many things this semester!\n");
+ 
+    return 0;
+}
